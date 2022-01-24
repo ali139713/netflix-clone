84808264dc19f84a9b07e1e45b8c2e82a8b6f2a7
Ali Abdullah <aliabdullah@aquila360.com>
2022-01-24 21:36:20 +0500

    Removed context.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,9 @@ import './App.css';
 import Banner from './components/Banner';
 import Nav from './components/Nav';
 import Row from './components/Row';
-import { LoaderProvider, useLoader } from './Context/context';
 import requests from './requests';
 
 function App() {
-  const { loading } = useLoader();
-
   var options = {
     lines: 13,
     length: 20,
@@ -32,21 +29,19 @@ function App() {
   };
 
   return (
-    <LoaderProvider>
-      <div className="App">
-        {loading && <ReactLoader loaded={false} options={options} className="loader" />}
-        <Nav />
-        <Banner />
-        <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow></Row>
-        <Row title="Trending Now" fetchUrl={requests.fetchTrending}></Row>
-        <Row title="Top Rated" fetchUrl={requests.fetchTopRated}></Row>
-        <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}></Row>
-        <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}></Row>
-        <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}></Row>
-        <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}></Row>
-        <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}></Row>
-      </div>
-    </LoaderProvider>
+    <div className="App">
+      <ReactLoader loaded={false} options={options} className="loader" />
+      <Nav />
+      <Banner />
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow></Row>
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}></Row>
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}></Row>
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}></Row>
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}></Row>
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}></Row>
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}></Row>
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}></Row>
+    </div>
   );
 }
 
